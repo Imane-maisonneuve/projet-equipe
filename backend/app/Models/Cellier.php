0960f06135f01375aa7c60aa60267f45d20142c9
imane-new-me <i.elmostaqim.ca@gmail.com>
2026-04-07 14:47:28 -0400

    ST-36 Creer le model et la migration de la table Cellier

---
+++ backend/app/Models/Cellier.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cellier extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'usager_id',
+        'nom',
+    ];
+
+    // ajouter la relation avec Usager
+    public function usager()
+    {
+        return $this->belongsTo(Usager::class);
+    }
+
+    // ajouter la relation avec CellierVin
+    // public function cellier_vin()
+    // {
+    //     return $this->hasMany(CellierVin::class);
+    // }
+}
